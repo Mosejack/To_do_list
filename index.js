7b9da10110ee82145ff211e1d74c12f6a3a0fc6e
mosejack <moseja0147@comcast.net>
2023-05-23 11:14:42 -0700

    update to js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function addItem(list, inputField) { 
     var list = document.getElementById(list);
     var listItem = document.createElement("li");
-    listItem.innerText = inputField.value; // passed the field. 
+    listItem.innerText = inputField.value; 
     list.appendChild(listItem);
-    return false; // stop submission
+    inputField.value = '';
+    return false; 
   }
   
 console.log('My code is running');
